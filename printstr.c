4bcade0f585592712e82cd357f894385ae94731d
Cristina Manzanares Urrutia <crmanzan@car5s2.42barcelona.com>
2023-10-18 19:03:30 +0200

    por si aca

--- printstr.c
+++ printstr.c
@@ -6,11 +6,12 @@
 /*   By: crmanzan <crmanzan@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/05 16:46:29 by crmanzan          #+#    #+#             */
-/*   Updated: 2023/10/18 18:39:01 by crmanzan         ###   ########.fr       */
+/*   Updated: 2023/10/18 18:57:17 by crmanzan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <printf.h>
+#include <unistd.h>
 
 int	printstr(char *str)
 {
